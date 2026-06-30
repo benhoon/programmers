d1b15e671bc01342fe9c7b4127bc83e53ad441c0
이상훈 <50623762+benhoon@users.noreply.github.com>
2026-06-30 14:58:42 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    check = [0,1,2,3,4,5,6,7,8,9]
+    answer = 0
+    for i in check:
+        if(i in numbers):
+            continue
+        else:
+            answer += i
+    return answer
